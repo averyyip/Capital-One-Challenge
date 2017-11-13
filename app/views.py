66dcafd255c6c74a6ec98f9df850796749fe3694
Avery Yip <averyyip@ymail.com>
2017-11-13 05:10:57 -0800

    Fixed bugs and completed project

---
+++ app/views.py
@@ -0,0 +1,22 @@
+from app import app
+from flask import render_template, flash, redirect, request
+from estimates import *
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html", estimate_price="", estimate_weekly_revenue="", scroll=None)
+
+@app.route('/handle_rev', methods=['POST'])
+def handle_rev():
+    lat = request.form['lat']
+    lon = request.form['lon']
+    rev = "$" + str(round(estimate_weekly_rev(lat,lon), 2))
+    return render_template("index.html", estimate_price="", estimate_weekly_revenue=rev, scroll="rev")
+
+@app.route('/handle_price', methods=['POST'])
+def handle_price():
+    lat = request.form['lat']
+    lon = request.form['lon']
+    price = "$" + str(round(estimate_ideal_price(lat,lon), 2))
+    return render_template("index.html", estimate_price=price, estimate_weekly_revenue="", scroll="price")
---
+++ estimates.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+import re
+
+def clean_neighbourhood(text):
+    text = re.sub(r"/|\s|-|'", "", text.lower())
+    if text == "civiccenter" or text == "tenderloin" or text == "unionsquare":
+        return "downtown"
+    elif text == "westernadditionnopa" or text == "alamosquare" or text == "fillmoredistrict" or text == "hayesvalley" or text == "japantown" or text == "lowerhaight":
+        return "westernaddition"
+    elif text == "dogpatch":
+        return "potrerohill"
+    elif text == "balboaterrace" or text == "foresthill" or text == "westportal":
+        return "westoftwinpeaks"
+    elif text == "colevalley":
+        return "haightashbury"
+    elif text == "cowhollow":
+        return "marina"
+    elif text == "dubocetriangle":
+        return "thecastro"
+    elif text == "fishermanswharf" or text == "telegraphhill":
+        return "northbeach"
+    elif text == "ingleside":
+        return "oceanview"
+    elif text == "missionbay":
+        return "soma"
+    elif text == "sunnyside":
+        return "outermission"
+    elif text == "portola":
+        return "excelsior"
+    elif text == "southbeach":
+        return "financialdistrict"
+    else:
+        return text
+
+def get_neighborhood(lat, lon):
+	lat = float(lat)
+	lon = float(lon)
+	listings = pd.read_csv("airbnb-sep-2017/listings.csv") 
+	lat_lon = listings[["latitude", "longitude", "neighbourhood"]].dropna()
+	print(lat_lon[:1])
+	distances = np.sqrt((lat_lon["latitude"] - lat) ** 2 + (lat_lon["longitude"] - lon) ** 2)
+	min_idx = np.argmin(distances)
+	lat_lon["neighbourhood"] = lat_lon["neighbourhood"].apply(clean_neighbourhood)
+	return lat_lon.loc[min_idx, "neighbourhood"]
+
+def estimate_weekly_rev(lat, lon):
+	neighbourhood = get_neighborhood(lat, lon)
+	results = pd.read_csv("avg_weekly_revenue_neighbourhood.csv")
+	return results[results["neighbourhood"] == neighbourhood].iloc[0,3]
+
+def estimate_ideal_price(lat, lon):
+	neighbourhood = get_neighborhood(lat, lon)
+	results = pd.read_csv("max_rev.csv")
+	return results[results["neighbourhood"] == neighbourhood].iloc[0,3] 
+
